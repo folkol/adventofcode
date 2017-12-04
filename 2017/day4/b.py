b5cae26faa773a25768bc8c44f7c67b9a1e7d9ad
Matte <mattias4@kth.se>
2017-12-04 18:51:24 +0100

    Simplified validation, anagram-check will also cover the duplicates.

--- 2017/day4/b.py
+++ 2017/day4/b.py
@@ -1,6 +1,5 @@
 """Counts passphrases as defined by https://adventofcode.com/2017/day/4."""
 import fileinput
-from collections import Counter
 from itertools import permutations
 
 
@@ -8,13 +7,8 @@ def valid_passphrase(passphrase):
     def is_anagram(word1, word2):
         return sorted(word1) == sorted(word2)
 
-    cardinalities = Counter(passphrase)
-    no_duplicates = all(cardinality == 1 for cardinality in cardinalities.values())
-    words = cardinalities.keys()
-    no_anarams = not any(is_anagram(word1, word2) for word1, word2 in permutations(words, r=2))
-
-    return no_duplicates and no_anarams
+    return not any(is_anagram(word1, word2) for word1, word2 in permutations(passphrase, r=2))
 
 
-phrases = (Counter(line.split()) for line in fileinput.input())
+phrases = (line.split() for line in fileinput.input())
 print(sum(1 for phrase in phrases if valid_passphrase(phrase)))

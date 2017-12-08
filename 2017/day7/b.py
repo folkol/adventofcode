613a40ee30ffd8278def8b5c2da88801f92da6a1
Matte <mattias4@kth.se>
2017-12-08 20:24:49 +0100

    Some more refactoring

--- 2017/day7/b.py
+++ 2017/day7/b.py
@@ -1,55 +1,65 @@
-"""Searches for the node with the wrong weight.
+"""Finds the solution for https://adventofcode.com/2017/day/7.
 
-    Unbalanced nodes are nodes with a recursive weight that differs
-    from its siblings.
+    Constructs a tree graph of the nodes read from stdin, finds the node with
+    a divergent weight and prints its ideal weight to stdout.
 
-    From these nodes, the one with balanced children is the one with
-    the wrong weight.
+    The tree is defined by a number of lines such as this one:
 
-    Todo: Add proper data structures to avoid looksups all the time.
+        name (weight) -> child1, child2...
 """
 import re
 from collections import defaultdict, Counter
+from sys import stdin
 
 
 class Node(object):
-    def total_weight(self):
-        """Recursively calculate the weight for all programs in this node."""
-        return self.weight + sum(child.total_weight() for child in self.children)
-
     def __init__(self):
         self.name = 'unknown'
         self.weight = 0
         self.parent = None
         self.children = []
 
-
-nodes = defaultdict(Node)
-# for line in stdin:
-for line in open('programs.dat'):
-    name, weight, *children = re.findall('\w+', line)
-    node = nodes[name]
-    node.name = name
-    node.weight = int(weight)
-    for child in children:
-        child_node = nodes[child]
-        child_node.parent = node
-        child_node.name = child
-        node.children.append(nodes[child])
+    def total_weight(self):
+        """Total weight of this node and its children, recursively."""
+        return self.weight + sum(child.total_weight() for child in self.children)
 
 
-def unbalanced(children):
-    counter = Counter(child.total_weight() for child in children)
-    if len(counter) == 1:
+def read_graph(f):
+    graph = defaultdict(Node)
+    for line in f:
+        name, weight, *children = re.findall('\w+', line)
+        node = graph[name]
+        node.name = name
+        node.weight = int(weight)
+        for child in children:
+            child_node = graph[child]
+            child_node.parent = node
+            child_node.name = child
+            node.children.append(child_node)
+    return dict(graph)
+
+
+def deviant(children):
+    """Returns the deviant child, of any."""
+    weights = Counter(child.total_weight() for child in children)
+    if len(weights) == 1:
         return None
-    (mode, _), (wrong, _) = counter.most_common()
+    (mode, _), (outlier, _) = weights.most_common()
     for child in children:
-        if child.total_weight() == wrong:
+        if child.total_weight() == outlier:
             return child
 
 
-for name, node in nodes.items():
-    if node.parent and node == unbalanced(node.parent.children) and not unbalanced(node.children):
-        counter = Counter(child.total_weight() for child in node.parent.children)
-        (mode, _), *_ = counter.most_common()
-        print(mode)
+programs = read_graph(stdin)
+for program in programs.values():
+    if not program.parent:
+        # In solitude, balance!
+        continue
+
+    # A deviant node with no deviant children is the black sheep.
+    siblings = program.parent.children
+    if program is deviant(siblings) and not deviant(program.children):
+        deviant_node = program
+        counter = Counter(child.total_weight() for child in deviant_node.parent.children)
+        (ideal_weight, _), *_ = counter.most_common()
+        print(ideal_weight)

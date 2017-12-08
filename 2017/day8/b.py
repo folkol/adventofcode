1e2076eca36aa5cf1008c4eaf135d66b73e33e5c
Matte <mattias4@kth.se>
2017-12-08 22:39:05 +0100

    Part 2...

---
+++ 2017/day8/b.py
@@ -0,0 +1,25 @@
+import re
+from collections import defaultdict
+from operator import ge, lt, eq, gt, le, ne
+
+relational_operators = {
+    '>=': ge,
+    '<': lt,
+    '==': eq,
+    '>': gt,
+    '<=': le,
+    '!=': ne
+}
+
+highest_value = float('-inf')
+registers = defaultdict(int)
+for instruction in open('instructions.dat'):
+    reg1, op, val, _, reg2, rel, immediate = re.findall('\S+', instruction)
+    if relational_operators[rel](registers[reg2], int(immediate)):
+        if op == 'inc':
+            registers[reg1] += int(val)
+        else:
+            registers[reg1] -= int(val)
+    highest_value = max(registers[reg1], highest_value)
+
+print(highest_value)

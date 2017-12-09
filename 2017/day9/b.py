36c8165ec7bcc8ec1c7ebc38005a515e76e1e4d0
Matte <mattias4@kth.se>
2017-12-09 14:51:00 +0100

    Now counting garbage chars

--- 2017/day9/b.py
+++ 2017/day9/b.py
@@ -9,21 +9,24 @@ def as_chars(f):
 depth = 0
 score = 0
 inside_garbage = False
+garbage_count = 0
 chars = as_chars(open('stream.dat'))
 for char in chars:
     if char == '!':
         next(chars)
         continue
 
-    if char == '<':
+    if not inside_garbage and char == '<':
         inside_garbage = True
     elif inside_garbage:
         if char == '>':
             inside_garbage = False
+        else:
+            garbage_count += 1
     elif char == '{':
         depth += 1
     elif char == '}':
         score += depth
         depth -= 1
 
-print(score)
+print(garbage_count)

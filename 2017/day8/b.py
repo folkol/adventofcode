beee0fd833fb59902a40cf876397ff5f8a207559
Matte <mattias4@kth.se>
2017-12-09 12:54:12 +0100

    Using stdin again

--- 2017/day8/b.py
+++ 2017/day8/b.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from math import inf
 from operator import ge, lt, eq, gt, le, ne
+from sys import stdin
 
 relational_operators = {
     '>=': ge,
@@ -13,7 +14,7 @@ relational_operators = {
 
 highest_value = -inf
 registers = defaultdict(int)
-for instruction in open('instructions.dat'):
+for instruction in stdin:
     reg, op, val, _, cond_reg, rel, cond_val = instruction.split()
     if relational_operators[rel](registers[cond_reg], int(cond_val)):
         if op == 'inc':

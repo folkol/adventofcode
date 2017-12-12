a6dfe5d81288a8b2503e4063ff97d605d7ec0b51
Matte <mattias4@kth.se>
2017-12-12 15:18:37 +0100

    Some refactoring

--- 2017/day12/a.py
+++ 2017/day12/a.py
@@ -2,26 +2,25 @@ import re
 from collections import defaultdict
 
 
-def travel(n, edges, seen):
+def travel(n, nodes, seen):
     """Recursively visits every node in the graph defined by edges.
 
     The visited nodes will be accumulated in the set 'seen'.
     """
-    for neighbour in edges[n]:
-        if neighbour not in seen:
-            seen.add(neighbour)
-            travel(neighbour, edges, seen)
+    for node in nodes[n]:
+        if node not in seen:
+            seen.add(node)
+            travel(node, nodes, seen)
 
 
-# Builds up an adjacency list by reading connections.dat
-edges = defaultdict(list)
+# Builds up an adjacency list
+nodes = defaultdict(list)
 for line in open('connections.dat'):
     node, *neighbours = re.findall('\d+', line)
     for neighbour in neighbours:
-        edges[node].append(neighbour)
-        edges[neighbour].append(node)
+        nodes[node].append(neighbour)
+        nodes[neighbour].append(node)
 
-
-seen = set()
-travel('0', edges, seen)
-print(len(seen))
+group = set()
+travel('0', nodes, group)
+print(len(group))
--- 2017/day12/b.py
+++ 2017/day12/b.py
@@ -2,32 +2,28 @@ import re
 from collections import defaultdict
 
 
-def travel(n, edges, seen):
+def travel(n, nodes, seen):
     """Recursively visits every node in the graph defined by edges.
 
     The visited nodes will be accumulated in the set 'seen'.
     """
-    for neighbour in edges[n]:
-        if neighbour not in seen:
-            seen.add(neighbour)
-            travel(neighbour, edges, seen)
+    for node in nodes[n]:
+        if node not in seen:
+            seen.add(node)
+            travel(node, nodes, seen)
 
 
 # Builds up an adjacency list by reading connections.dat
-edges = defaultdict(list)
+nodes = defaultdict(list)
 for line in open('connections.dat'):
     node, *neighbours = re.findall('\d+', line)
-    for neighbour in neighbours:
-        edges[node].append(neighbour)
-        edges[neighbour].append(node)
-
+    nodes[node].extend(neighbours)
 
 num_groups = 0
-
 seen = set()
-while len(set(edges) - set(seen)):
-    unseen = next(iter(set(edges) - set(seen)))
-    travel(unseen, edges, seen)
+while len(set(nodes) - set(seen)):
+    unseen = next(n for n in nodes if n not in seen)
+    travel(unseen, nodes, seen)
     num_groups += 1
 
 print(num_groups)

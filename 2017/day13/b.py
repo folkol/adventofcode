f03d46d5e924d92f34633c7774f5639202a6297d
Matte <mattias4@kth.se>
2017-12-13 23:01:43 +0100

    Replaced some magic expressions with function calls. Also, breaks early on capture.

--- 2017/day13/b.py
+++ 2017/day13/b.py
@@ -1,21 +1,22 @@
 import re
+from itertools import count
 
 rows = (re.findall('\d+', line) for line in open('layers.dat'))
 layers = {int(k): int(v) for k, v in rows}
-print(layers)
 
-delay = 0
-while True:
-    severity = 0
-    for pos in range(max(layers) + 1):
-        if pos not in layers:
-            continue
-        n = layers[pos]
-        scanner_pos = (delay + pos) % (2 + (n - 2) * 2)
-        if scanner_pos == 0:
-            # print(f'Caught at depth {pos}')
-            severity = 1
-    if severity == 0:
-        print(f'Got through unharmed with a delay of {delay}')
+
+def scanner_pos(delay, step):
+    def route_len():
+        return 2 + (layers[step] - 2) * 2
+
+    return (delay + step) % route_len()
+
+
+for delay in count():
+    for step in range(max(layers) + 1):
+        if step in layers and scanner_pos(delay, step) == 0:
+            break
+    else:
         break
-    delay += 1
+
+print(delay)

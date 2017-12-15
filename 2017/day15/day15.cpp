5df3c2c4e64ae670b6628334c03e49a15cf3339c
Matte <mattias4@kth.se>
2017-12-15 17:52:20 +0100

    Added comments about linear_congruential_engine

--- 2017/day15/day15.cpp
+++ 2017/day15/day15.cpp
@@ -7,8 +7,9 @@ const int N = 40000000;
 using namespace std;
 
 int main() {
-    minstd_rand0 A(634);
-    minstd_rand B(301);
+    // std::linear_congruential_engine<T, a, c, m>
+    minstd_rand0 A(634); // uint_fast32_t, 16807, 0, 2147483647
+    minstd_rand  B(301); // uint_fast32_t, 48271, 0, 2147483647
 
     int score = 0;
     for (int i = 0; i < N; i++) {

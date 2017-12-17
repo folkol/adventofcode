36202a72ee5df481e6edcb701ccc8b471064e47f
Matte <mattias4@kth.se>
2017-12-17 18:13:54 +0100

    removed printout

--- 2017/day17/a.py
+++ 2017/day17/a.py
@@ -5,6 +5,5 @@ pos = 0
 for n in range(50_000_000):
     pos = ((pos + steps) % len(buffer)) + 1
     buffer.insert(pos, n + 1)
-    print(buffer.index(0), len(buffer), buffer[buffer.index(0) + 1])
 
 print(buffer[pos + 1])

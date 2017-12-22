600cce1654db15bdbd45ca23e314cbfb5928a198
Matte <mattias4@kth.se>
2017-12-22 21:02:32 +0100

    Cleaned up part a

--- 2017/day19/a.py
+++ 2017/day19/a.py
@@ -7,46 +7,26 @@ DOWN = Direction(0, 1)
 LEFT = Direction(-1, 0)
 RIGHT = Direction(1, 0)
 
-left = {
-    UP: LEFT,
-    DOWN: RIGHT,
-    LEFT: DOWN,
-    RIGHT: UP
-}
-
-right = {
-    UP: RIGHT,
-    DOWN: LEFT,
-    LEFT: UP,
-    RIGHT: DOWN
-}
+turn_left = {UP: LEFT, DOWN: RIGHT, LEFT: DOWN, RIGHT: UP}
+turn_right = {UP: RIGHT, DOWN: LEFT, LEFT: UP, RIGHT: DOWN}
 
 diagram = [list(row.strip('\n')) for row in open('diagram.dat')]
-# diagram = [list(row.strip('\n')) for row in open('example.dat')]
+
 x, y = diagram[0].index('|'), -1
 direction = DOWN
 
 seen = []
-
-
-
 while True:
-    # print(x, y)
-    # print('\n'.join(row for row in (''.join(cs) for cs in diagram)))
-    point = diagram[y][x]
-    if point in ascii_uppercase:
-        seen.append(point)
+    if diagram[y][x] in ascii_uppercase:
+        seen.append(diagram[y][x])
 
-    next_x, next_y = x + direction.dx, y + direction.dy
-    ahead = diagram[next_y][next_x]
-    if ahead in '-|' + ascii_uppercase:
-        x, y = next_x, next_y
-    elif ahead == '+':
-        x, y = next_x, next_y
-        next_direction = left[direction]
-        if diagram[y + next_direction.dy][x + next_direction.dx] not in '-|' + ascii_uppercase:
-            next_direction = right[direction]
-        direction = next_direction
-    else:
+    x, y = x + direction.dx, y + direction.dy
+    if diagram[y][x] == ' ':
         break
+    elif diagram[y][x] == '+':
+        direction = turn_left[direction]
+        if diagram[y + direction.dy][x + direction.dx] == ' ':
+            direction = turn_right[direction]
+            direction = turn_right[direction]
+
 print(''.join(seen))

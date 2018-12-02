543bf9cc4e6ea83c0a6f819ed63157ed53f3304a
Matte <mattias4@kth.se>
2018-12-02 16:13:47 +0100

    Gave the fugly string join a name.

--- 2018/day2/b.py
+++ 2018/day2/b.py
@@ -3,11 +3,15 @@ from itertools import combinations
 from typing import Sequence
 
 
+def common_letters(a, b):
+    return ''.join(i for i, j in zip(a, b) if i == j)
+
+
 def find_similar(box_ids: Sequence[str]):
     for a, b in combinations(box_ids, r=2):
         distance = sum(i != j for i, j in zip(a, b))
         if distance == 1:
-            return ''.join(i for i, j in zip(a, b) if i == j)
+            return common_letters(a, b)
 
 
 print(find_similar(fileinput.input()))

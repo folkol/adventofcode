76a4140752669aa5a9bf97461a8a5a9b8d1a245a
Matte <mattias4@kth.se>
2018-12-02 13:14:26 +0100

    Renamed find_boxes -> find_similar, added type annotation since we join on the elements.

--- 2018/day2/b.py
+++ 2018/day2/b.py
@@ -1,12 +1,13 @@
 import fileinput
 from itertools import combinations
+from typing import Sequence
 
 
-def find_boxes(box_ids):
+def find_similar(box_ids: Sequence[str]):
     for a, b in combinations(box_ids, r=2):
         distance = sum(i != j for i, j in zip(a, b))
         if distance == 1:
             return ''.join(i for i, j in zip(a, b) if i == j)
 
 
-print(find_boxes(fileinput.input()))
+print(find_similar(fileinput.input()))

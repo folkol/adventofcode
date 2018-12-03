4b0bd1ddc60091a7ab57694973d37fe8d8233776
Matte <mattias.johansson@ivbar.com>
2018-12-03 22:24:34 +0100

    Part a

---
+++ 2018/day3/a.py
@@ -0,0 +1,15 @@
+import fileinput
+import re
+from collections import defaultdict
+
+squares = defaultdict(int)
+for line in fileinput.input():
+    claim, x, y, width, height = (int(n) for n in re.findall('(\d+)', line))
+    for i in range(width):
+        for j in range(height):
+            squares[x + i, y + j] += 1
+
+print(sum(x > 1 for x in squares.values()))
+
+
+

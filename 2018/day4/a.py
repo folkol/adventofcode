57cf971c1523b75a2094bfe160cb0385b03157e9
Matte <mattias4@kth.se>
2018-12-06 21:42:07 +0100

    Some touch-ups on Day 4.

--- 2018/day4/a.py
+++ 2018/day4/a.py
@@ -3,30 +3,34 @@ from collections import defaultdict, Counter
 from datetime import datetime, timedelta
 
 
-def date_range(begin, end):
-    while begin < end:
-        yield begin
-        begin += timedelta(minutes=1)
+def date_range(first, last):
+    while first < last:
+        yield first
+        first += timedelta(minutes=1)
+
+
+def sleepyness(item):
+    """Returns the number of minutes a guard spent sleeping on duty."""
+    _, minutes = item
+    return len(minutes)
 
 
 with open('data.dat') as f:
     data = sorted(f.readlines())
 
 guards = defaultdict(list)
-
-fell_asleep = guard = None
+guard = fell_asleep = None
 for line in data:
     date = datetime.strptime(line[1:17], '%Y-%m-%d %H:%M')
     match = re.match('.*Guard #(\d+) begins shift', line)
     if match:
-        guard = match.group(1)
+        guard = int(match.group(1))
     if 'falls asleep' in line:
         fell_asleep = date
     if 'wakes up' in line:
         for d in date_range(fell_asleep, date):
             guards[guard].append(d.minute)
 
-guard, minutes = max(guards.items(), key=lambda x: len(x[1]))
-minute, count = Counter(minutes).most_common(1)[0]
-
-print(int(guard) * int(minute))
+guard, minutes = max(guards.items(), key=sleepyness)
+minute = Counter(minutes).most_common(1)[0][0]
+print(guard * minute)
--- 2018/day4/b.py
+++ 2018/day4/b.py
@@ -3,37 +3,33 @@ from collections import defaultdict, Counter
 from datetime import datetime, timedelta
 
 
-def date_range(begin, end):
-    while begin < end:
-        yield begin
-        begin += timedelta(minutes=1)
+def date_range(first, last):
+    while first < last:
+        yield first
+        first += timedelta(minutes=1)
+
+
+def most_sleep_per_minute(arg):
+    guard, minutes = arg
+    return Counter(minutes).most_common(1)[0][1]
 
 
 with open('data.dat') as f:
     data = sorted(f.readlines())
 
 guards = defaultdict(list)
-
-fell_asleep = guard = None
+guard = fell_asleep = None
 for line in data:
     date = datetime.strptime(line[1:17], '%Y-%m-%d %H:%M')
     match = re.match('.*Guard #(\d+) begins shift', line)
     if match:
-        guard = match.group(1)
+        guard = int(match.group(1))
     if 'falls asleep' in line:
         fell_asleep = date
     if 'wakes up' in line:
         for d in date_range(fell_asleep, date):
             guards[guard].append(d.minute)
 
-
-def most_sleepy_minute(arg):
-    guard, minutes = arg
-    _, count = Counter(minutes).most_common(1)[0]
-    return count
-
-
-guard, minutes = max(guards.items(), key=most_sleepy_minute)
-minute, _ = Counter(minutes).most_common(1)[0]
-
-print(int(guard) * int(minute))
+guard, minutes = max(guards.items(), key=most_sleep_per_minute)
+minute = Counter(minutes).most_common(1)[0][0]
+print(guard * minute)

ab1e8e2caf02bbb8ab7fdc245c090a29deca8754
Matte <mattias4@kth.se>
2018-12-07 22:54:37 +0100

    Cleaned up part b.

--- 2018/day7/b.py
+++ 2018/day7/b.py
@@ -10,30 +10,28 @@ with open('steps.dat') as f:
     for line in f:
         match = re.match('Step (.) must be finished before step (.) can begin.', line)
         dependency, step = match.groups()
-        steps.add(dependency)
-        steps.add(step)
+        steps.update(dependency, step)
         dependencies[step].add(dependency)
 
 order = []
-
 workers = {worker: '.' for worker in range(1, 6)}
 time = 0
 queue = []
 enqueued = []
 
-while True:
+for time in count():
     while queue and queue[0][0] <= time:
         worker, task = heappop(queue)[2]
         workers[worker] = '.'
         order.append(task)
     if len(order) == len(steps):
         break
-    candidates = {step for step in steps if step not in enqueued and dependencies[step] <= set(order)}
+    candidates = {s for s in steps if s not in enqueued and dependencies[s] <= set(order)}
     idle_workers = {worker for worker, task in workers.items() if task == '.'}
-    for candidate, worker in zip(sorted(candidates), sorted(idle_workers)):
+    for candidate, worker in zip(sorted(candidates), idle_workers):
         enqueued.append(candidate)
         workers[worker] = candidate
-        heappush(queue, (time + 60 + ord(candidate) - 64, next(counter), (worker, candidate)))
+        heappush(queue, (time + ord(candidate) - 4, next(counter), (worker, candidate)))
     print(time, *workers.values(), ''.join(order), sep='\t')
-    time += 1
-print(time)
+
+print(f'Assembly done in {time} seconds!')

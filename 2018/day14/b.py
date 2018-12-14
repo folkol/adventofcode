bb86e50d5f2aaee5fa8683a9f37d6824e8fb0311
Matte <mattias4@kth.se>
2018-12-14 16:56:32 +0100

    Part b

--- 2018/day14/b.py
+++ 2018/day14/b.py
@@ -2,12 +2,14 @@ N = [8, 4, 6, 6, 0, 1]
 scores = [3, 7]
 
 elf1, elf2 = 0, 1
-while True:
-    ds = (int(n) for n in str(scores[elf1] + scores[elf2]))
-    for d in ds:
+found = None
+while not found:
+    for d in (int(d) for d in str(scores[elf1] + scores[elf2])):
         scores.append(d)
-        if scores[-6:] == N:
-            print('Found it @', len(scores) - 6)
+        if scores[-len(N):] == N:
+            found = len(scores) - len(N)
             break
     elf1 = (elf1 + 1 + scores[elf1]) % len(scores)
     elf2 = (elf2 + 1 + scores[elf2]) % len(scores)
+
+print('Found it at', found)

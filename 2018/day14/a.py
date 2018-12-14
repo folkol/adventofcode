578eccadc59676a6bf8e163f2ffe297736f6f49d
Matte <mattias4@kth.se>
2018-12-14 16:47:07 +0100

    Part a

--- 2018/day14/a.py
+++ 2018/day14/a.py
@@ -4,9 +4,7 @@ scores = [3, 7]
 elf1, elf2 = 0, 1
 while len(scores) < (N + 10):
     scores.extend(int(n) for n in str(scores[elf1] + scores[elf2]))
-    elf1 += 1 + scores[elf1]
-    elf1 %= len(scores)
-    elf2 += 1 + scores[elf2]
-    elf2 %= len(scores)
+    elf1 = (elf1 + 1 + scores[elf1]) % len(scores)
+    elf2 = (elf2 + 1 + scores[elf2]) % len(scores)
 
 print(*scores[N:N + 10], sep='')

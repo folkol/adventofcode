40860227513483371665a00a221efe7874417ed7
Matte <mattias4@kth.se>
2018-12-16 14:17:32 +0100

    Cleaned up Part a.

--- 2018/day15/a.py
+++ 2018/day15/a.py
@@ -1,111 +1,105 @@
 import sys
-from collections import deque, defaultdict
+from collections import deque
 from itertools import count
-
-adjacents = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
 with open('cave.dat') as f:
     mobs = {}
-    map = {}
+    cave = {}
     for y, line in enumerate(f):
         for x, cell in enumerate(line.rstrip('\n')):
             if cell in 'EG':
                 mobs[(x, y)] = [cell, 200]
                 cell = '.'
-            map[(x, y)] = cell
+            cave[(x, y)] = cell
 
 
-def show():
-    global y, x, cell
-    for y in range(9):
-        for x in range(9):
+def plot_cave():
+    for y in range(max(y for _, y in cave) + 1):
+        for x in range(max(x for x, _ in cave) + 1):
             cell = mobs.get((x, y))
             if cell:
                 print(cell[0], end='')
             else:
-                print(map.get((x, y), ' '), end='')
+                print(cave.get((x, y), ' '), end='')
         print()
-    print(*mobs.values())
 
 
 def reading_order(item):
     try:
         (x, y), cell = item
-    except:
+    except TypeError:
         (x, y) = item
     return y, x
+
+
+def done():
+    full_turns = round - 1
+    hps = [hp for _, hp in mobs.values()]
+    hp = sum(hps)
+    assert full_turns * hp == 190777, (full_turns, hp)
+    plot_cave()
+    print(f'Combat over after {full_turns} full turns. Total hp was {hp} and outcome was {full_turns * hp}')
+    sys.exit(0)
+
+
+def adjacents(x, y):
+    return [(x + dx, y + dy) for (dx, dy) in [(0, -1), (0, 1), (-1, 0), (1, 0)]]
+
+
+def unoccupied(cell):
+    return cave.get(cell) == '.' and cell not in mobs
+
+
+def bfs(start, current):
+    """Breadth-first scan of the empty + current cells, returns a map of distances to each cell."""
+
+    q = deque()
+    q.append(start)
+    distances = {start: 0}
+    while q:
+        cell = q.popleft()
+        for adjacent in [c for c in adjacents(*cell) if c == current or unoccupied(c)]:
+            if adjacent not in distances:
+                q.append(adjacent)
+                distances[adjacent] = distances[cell] + 1
+    return distances
+
+
+def is_dead(mob):
+    return mob[1] < 1
 
 
 for round in count(1):
     for (x, y), current in sorted(mobs.items(), key=reading_order):
         # print(f'{x}, {y}, {current}: ')
-        if current[1] < 1:
+        if is_dead(current):
             continue
         enemies = [pos for pos, mob in mobs.items() if mob[0] != current[0]]
         if not enemies:
-            full_turns = round - 1
-            hps = [hp for _, hp in mobs.values()]
-            print(*hps)
-            hp = sum(hps)
-            print(f'Combat over after {full_turns} full turns. Total hp was {hp} and outcome was {full_turns * hp}')
-            show()
-            sys.exit(0)
-        targets = [(x + dx, y + dy)
-                   for x, y in enemies
-                   for (dx, dy) in adjacents
-                   if map.get((x + dx, y + dy)) == '.']
+            done()
+        targets = [(dx, dy) for x, y in enemies for (dx, dy) in adjacents(x, y) if cave.get((dx, dy)) == '.']
         if not targets:
             continue
-        if not any(foo in enemies for foo in [(x + dx, y + dy) for dx, dy in adjacents]):
-            q = deque()
-            q.append((x, y))
-            visited = {(x, y): 1}
-            parents = defaultdict(list)
-            while q:
-                node = q.popleft()
-                for adjacent in [(node[0] + dx, node[1] + dy)
-                                 for (dx, dy) in adjacents
-                                 if map.get((node[0] + dx, node[1] + dy)) == '.'
-                                    and (node[0] + dx, node[1] + dy) not in mobs]:
-                    if adjacent not in visited:
-                        q.append(adjacent)
-                        visited[adjacent] = visited[node] + 1
-                        parents[adjacent].append(node)
-                    elif visited[node] == visited[adjacent]:
-                        parents[adjacent].append(node)
+        if not any(foo in enemies for foo in adjacents(x, y)):
+            visited = bfs((x, y), (x, y))
             in_visited = [visited.get(target) for target in targets if target in visited]
             if not in_visited:
                 continue
             distance = min(in_visited)
             nearest = [target for target in targets if target in visited and visited[target] == distance]
             chosen = sorted(nearest, key=reading_order)[0]
-            q = deque()
-            q.append(chosen)
-            visited = {chosen: 0}
-            while q:
-                node = q.popleft()
-                for adjacent in [(node[0] + dx, node[1] + dy)
-                                 for (dx, dy) in adjacents
-                                 if (node[0] + dx, node[1] + dy) == (x, y) or
-                                    (map.get((node[0] + dx, node[1] + dy)) == '.'
-                                     and (node[0] + dx, node[1] + dy) not in mobs)]:
-                    if adjacent not in visited:
-                        q.append(adjacent)
-                        visited[adjacent] = visited[node] + 1
-            steps = [(x + dx, y + dy) for (dx, dy) in adjacents if
-                     map.get((x + dx, y + dy)) == '.' and (x + dx, y + dy) not in mobs]
+            visited = bfs(chosen, (x, y))
+            steps = [(x, y) for x, y in adjacents(x, y) if cave.get((x, y)) == '.' and (x, y) not in mobs]
             step = sorted(steps, key=lambda x: (visited[x], *reading_order(x)))[0]
-            print(f'{round}: {(x, y)} -> {step}')
+            # print(f'{round}: {(x, y)} -> {step}')
             mobs[step] = current
             del mobs[(x, y)]
             (x, y) = step
-        adjacent_enemies = [(foo, mobs[foo]) for foo in [(x + dx, y + dy) for dx, dy in adjacents] if foo in enemies]
+        adjacent_enemies = [(foo, mobs[foo]) for foo in adjacents(x, y) if foo in enemies]
         if adjacent_enemies:
             target = sorted(adjacent_enemies, key=lambda e: (e[1][1], *reading_order(e)))[0]
             target[1][1] -= 3
-            print(f'{round}: ({x}, {y}) attacks {target}')
+            # print(f'{round}: ({x}, {y}) attacks {target}')
             if target[1][1] < 1:
-                print(f'{round}: {target} dies')
+                # print(f'{round}: {target} dies')
                 del mobs[target[0]]
-
-

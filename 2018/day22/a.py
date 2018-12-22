2a311622ef8dfa47cd0e19792764ab8d5cd5a103
Matte <mattias4@kth.se>
2018-12-22 12:23:48 +0100

    Cleaned up a a bit.

--- 2018/day22/a.py
+++ 2018/day22/a.py
@@ -1,56 +1,37 @@
 from collections import namedtuple
 from dataclasses import make_dataclass
+from itertools import product
 
 Coordinate = namedtuple('Coordinate', ('x', 'y'))
 Region = make_dataclass('Region', ('type', 'index', 'erosion'))
 
+mouth = 0, 0
 depth = 8112
 target = Coordinate(13, 743)
-# depth = 510
-# target = Coordinate(10, 10)
-
-cave = {}
-for pos in (Coordinate(x, y) for y in range(target.y + 1) for x in range(target.x + 1)):
-    if pos == (0, 0):
-        index = 0
-    elif pos == target:
-        index = 0
-    elif pos.y == 0:
-        index = pos.x * 16807
-    elif pos.x == 0:
-        index = pos.y * 48271
-    else:
-        index = cave[Coordinate(pos.x - 1, pos.y)].erosion * cave[Coordinate(pos.x, pos.y - 1)].erosion
-
-    erosion = (index + depth) % 20183
-
-    if erosion % 3 == 0:
-        region = 'rocky'
-    elif erosion % 3 == 1:
-        region = 'wet'
-    else:
-        region = 'narrow'
-
-    cave[pos] = Region(region, index, erosion)
-
-# for y in range(target[1] + 1 + 5):
-#     for x in range(target[0] + 1 + 5):
-#         pos = Coordinate(x, y)
-#         if pos == (0, 0):
-#             print('M', end='')
-#         elif pos == target:
-#             print('T', end='')
-#         elif cave[pos].type == 'rocky':
-#             print('.', end='')
-#         elif cave[pos].type == 'wet':
-#             print('=', end='')
-#         else:
-#             print('|', end='')
-#     print()
 
 risk_levels = {
     'rocky': 0,
     'wet': 1,
     'narrow': 2
 }
+region_type = {
+    0: 'rocky',
+    1: 'wet',
+    2: 'narrow'
+}
+cave = {}
+for pos in (Coordinate(x, y) for x, y in product(range(target.x + 1), range(target.y + 1))):
+    if pos in (mouth, target):
+        index = 0
+    elif pos.y == 0:
+        index = pos.x * 16807
+    elif pos.x == 0:
+        index = pos.y * 48271
+    else:
+        index = cave[pos.x - 1, pos.y].erosion * cave[pos.x, pos.y - 1].erosion
+
+    erosion = (index + depth) % 20183
+
+    cave[pos] = Region(region_type[erosion % 3], index, erosion)
+
 print(sum(risk_levels[region.type] for region in cave.values()))

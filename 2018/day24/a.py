614da573c8d809624ecfde3501e9b3143ba37207
Matte <mattias4@kth.se>
2018-12-26 18:14:27 +0100

    Part a works on test data.

--- 2018/day24/a.py
+++ 2018/day24/a.py
@@ -1,28 +1,37 @@
 import re
 from dataclasses import make_dataclass
 from itertools import count
+from pprint import pprint
 
-Unit = make_dataclass('Unit', ['id', 'number', 'hp', 'extras', 'dmg', 'dmg_type', 'initiative'])
+Unit = make_dataclass('Unit',
+                      ['army', 'id', 'number', 'hp', 'immunities', 'weaknesses', 'dmg', 'dmg_type', 'initiative'])
 
 ids = count()
 
 
 def parse_units(f):
     units = []
-    next(f)
+    army = next(f)[:-2]
     for line in f:
         if line == '\n':
             break
         base = re.match(r'(\d+) units each with (\d+) hit points ', line)
         line = line[base.span()[1]:]
         m = re.match(r'\((.*)\) ', line)
+        immunities = weaknesses = []
         if m:
             extras = m.group(1)
+            extras = extras.split('; ')
+            for extra in extras:
+                if extra.startswith('weak to'):
+                    weaknesses = extra[len('weak to '):].split(', ')
+                if extra.startswith('immune to '):
+                    immunities = extra[len('immune to '):].split(',')
             line = line[m.span()[1]:]
-        else:
-            extras = None
         combat = re.match(r'with an attack that does (\d+) (\w+) damage at initiative (\d+)', line)
-        units.append(Unit(next(ids), *base.groups(), extras, *combat.groups()))
+        dmg, dmg_type, initiative = combat.groups()
+        units.append(Unit(army, next(ids), *(int(g) for g in base.groups()), immunities, weaknesses, int(dmg), dmg_type,
+                          int(initiative)))
     return units
 
 
@@ -30,16 +39,59 @@ with open('units.dat') as f:
     immune_system = parse_units(f)
     infection = parse_units(f)
 
-print('Immune System:')
-for unit in immune_system:
-    print(f'{unit.number} units each with {unit.hp} hit points ', end='')
-    if unit.extras:
-        print(f'({unit.extras}) ', end='')
-    print(f'with an attack that does {unit.dmg} {unit.dmg_type} damage at initiative {unit.initiative}')
-print()
-print('Infection:')
-for unit in infection:
-    print(f'{unit.number} units each with {unit.hp} hit points ', end='')
-    if unit.extras:
-        print(f'({unit.extras}) ', end='')
-    print(f'with an attack that does {unit.dmg} {unit.dmg_type} damage at initiative {unit.initiative}')
+
+def expected_damage(unit):
+    def inner(enemy):
+        if unit.dmg_type in enemy.immunities:
+            ret = 0, int(enemy.number) * int(enemy.dmg) * 2, int(enemy.initiative)
+        elif unit.dmg_type in enemy.weaknesses:
+            ret = int(unit.number) * int(unit.dmg) * 2, int(enemy.number) * int(enemy.dmg), int(enemy.initiative)
+        else:
+            ret = int(unit.number) * int(unit.dmg), int(enemy.number) * int(enemy.dmg), int(enemy.initiative)
+        # print(f'{unit.id} would deal {enemy.id} {ret[0]} damage')
+        return ret
+
+    return inner
+
+
+# while True:
+
+
+def select_target(attackers, defenders, targets):
+    for unit in sorted(attackers, key=lambda unit: (int(unit.number) * int(unit.dmg), int(unit.initiative)),
+                       reverse=True):
+
+        enemies = sorted((d for d in defenders if d.id not in targets.values() and d.army != unit.army),
+                         key=lambda u: (expected_damage(unit)(u), u.dmg * u.number, u.initiative),
+                         reverse=True)
+        if enemies:
+            targets[unit.id] = enemies[0].id
+
+
+while True:
+    targets = {}
+    print('\nROUND')
+    select_target(immune_system, infection, targets)
+    select_target(infection, immune_system, targets)
+
+    for attacker in sorted((*immune_system, *infection), key=lambda unit: unit.initiative, reverse=True):
+        if attacker.hp <= 0:
+            continue
+        defender = next((unit for unit in (*immune_system, *infection) if unit.id == targets.get(attacker.id, None)),
+                        None)
+        if defender is None:
+            continue
+
+        dmg = expected_damage(attacker)(defender)
+        dealt = dmg[0] // int(defender.hp)
+        print(attacker.army, attacker.id, 'attacks', defender.id, min(defender.number, dealt))
+        defender.number -= dealt
+
+    immune_system = [u for u in immune_system if u.number > 0]
+    infection = [u for u in infection if u.number > 0]
+
+    if not immune_system or not infection:
+        break
+
+pprint(immune_system)
+pprint(infection)

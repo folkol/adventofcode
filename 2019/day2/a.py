83c0aed55036af3e141e35d7857aa5e25c61f2f6
Matte <mattias.johansson@ivbar.com>
2019-12-03 18:53:34 +0100

    Fooling around with op-mappings

--- 2019/day2/a.py
+++ 2019/day2/a.py
@@ -1,28 +1,30 @@
 from functools import partial
+import sys
 
 with open('input.dat') as f:
     data = f.read()
     program = list(map(int, data.split(',')))
 
-OP_ADD = 1
-OP_MUL = 2
-OP_HLT = 99
+def op_add(program, a, b, c):
+    program[c] = program[a] + program[b]
+
+def op_mul(program, a, b, c):
+    program[c] = program[a] * program[b]
+
+def op_hlt(program, *_):
+    print(f'program[0]: {program[0]}')  # 9706670
+    sys.exit(1)
+
+
+OP = {
+    1: op_add,
+    2: op_mul,
+    99: op_hlt
+}
 
 program[1], program[2] = 12, 2  # Restoring 1202 state
-
 PC = 0
 while True:
     op_code = program[PC]
-    if op_code == OP_ADD:
-        a, b, c = program[PC + 1: PC + 4]
-        program[c] = program[a] + program[b]
-    elif op_code == OP_MUL:
-        a, b, c = program[PC + 1: PC + 4]
-        program[c] = program[a] * program[b]
-    elif op_code == OP_HLT:
-        break
-    else:
-        print(f'Unknown op_code: {op_code}')
+    OP[op_code](program, *program[PC + 1: PC + 4])
     PC += 4
-
-print(program[0])  # 9706670

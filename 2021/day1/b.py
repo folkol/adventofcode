6b54ccc120a079ca8f5eab21ba5c11230839644b
Matte <mattias.johansson@ivbar.com>
2021-12-01 10:22:01 +0100

    Reimplemented windows-function with deque, also no longer handling edge cases #yolo

--- 2021/day1/b.py
+++ 2021/day1/b.py
@@ -1,19 +1,17 @@
 import sys
-from itertools import islice, tee
+from collections import deque
+from itertools import tee
 
-depths = (int(line) for line in sys.stdin)
-
-
-def windows(seq, n=2):
-    it = iter(seq)
-    result = tuple(islice(it, n))
-    if len(result) == n:
-        yield result
-    for elem in it:
-        result = result[1:] + (elem,)
-        yield result
+depths = [int(line) for line in sys.stdin]
 
 
-prev, curr = tee(sum(window) for window in windows(depths, 3))
+def windows(seq, n=3):
+    window = deque(seq[:n - 1], maxlen=n)
+    for item in seq[n - 1:]:
+        window.append(item)
+        yield tuple(window)
+
+
+prev, curr = tee(sum(window) for window in windows(depths))
 next(curr)
 print(sum(1 for i, j in zip(prev, curr) if j > i))

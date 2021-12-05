3f8a0e2fd36460c5d6e019a6c0cf04be8d0da1a1
Matte <mattias4@kth.se>
2021-12-05 20:51:55 +0100

    Day 5 b

---
+++ 2021/day5/b.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php -f
+<?php
+$grid = array();
+while($line = fgets(STDIN)){
+    [$a, $b, $c, $d] = array_map('intval', preg_split( '/,|(->)/', $line));
+    [$i, $j, $di, $dj] = [$a, $b, $c - $a <=> 0, $d - $b <=> 0];
+    $grid[strval($i) . ',' . strval($j)]++;
+    while($i != $c || $j != $d) {
+        $i += $di;
+        $j += $dj;
+        $grid[sprintf("%d,%d", $i, $j)]++;
+    };
+}
+echo array_reduce($grid, function($acc, $cell_count) {
+    return $acc + ($cell_count > 1);
+});
+?>

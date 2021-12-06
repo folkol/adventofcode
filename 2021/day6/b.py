be213f500661fb9e78f96c3bd3dc1559a93428a2
Matte <mattias4@kth.se>
2021-12-06 23:10:50 +0100

    Added comment about dp state

--- 2021/day6/b.py
+++ 2021/day6/b.py
@@ -1,6 +1,7 @@
 fish = [int(s) for s in input().split(',')]
+
 N = 256
-dp = [[0, 0] for _ in range(N + 10)]
+dp = [[0, 0] for _ in range(N + 10)]  # state: [fish, fishlings] * days
 dp[0][0] = len(fish)
 for n in fish:
     for m in range(n + 1, N + 1, 7):
@@ -10,4 +11,5 @@ for n in range(1, N + 1 + 9):
     dp[n][0] = dp[n - 1][0] + dp[n][1]
     for m in range(n + 9, N + 1, 7):
         dp[m][1] += dp[n][1]
+
 print(dp[N][0])

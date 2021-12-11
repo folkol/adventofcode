f039929f80ef3c70033fa09d7c27edd9527971b8
Matte <mattias4@kth.se>
2021-12-11 17:16:54 +0100

    Rewrote day 10 a from recursive to iterative

--- 2021/day10/a.c
+++ 2021/day10/a.c
@@ -1,73 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int total_points = 0;
+#define LPAREN(c) c == '(' || c == '[' || c == '{' || c == '<'
+typedef char c_pair[2];
+typedef struct { char bracket;int value; } bv;
 
-void count_points(char c) {
-    switch(c) {
-        case ')':
-            total_points += 3;
-            break;
-        case ']':
-            total_points += 57;
-            break;
-        case '}':
-            total_points += 1197;
-            break;
-        case '>':
-            total_points += 25137;
-            break;
+char complement(char c) {
+    static const c_pair table[] = {"()", "[]", "{}", "<>", ")(", "][", "}{", "><"};
+    for (int i = 0; i < sizeof(table) / sizeof(table[0]); i++) {
+        if (table[i][0] == c) {
+            return table[i][1];
+        }
     }
+    return 0;
 }
 
-int consume_chunk(char* line, int *pos, int limit) {
-    char end;
-    switch(line[*pos]) {
-        case '(':
-            end = ')';
-            break;
-        case '[':
-            end = ']';
-            break;
-        case '{':
-            end = '}';
-            break;
-        case '<':
-            end = '>';
-            break;
-        default:
-            return 0;
+int value(char c) {
+    static const bv table[] = {{')', 3}, {']', 57}, {'}', 1197}, {'>', 25137}};
+    for (int i = 0; i < sizeof(table) / sizeof(table[0]); i++) {
+        if (table[i].bracket == c) {
+            return table[i].value;
+        }
     }
-
-    (*pos)++;
-
-    while(consume_chunk(line, pos, limit))
-        ;
-
-    if(line[*pos] != end) {
-        count_points(line[*pos]);
-        *pos = limit;
-        return 0;
-    }
-
-    (*pos)++;
-
-    return 1;
+    return 0;
 }
-
 
 int main(void) {
+    int score = 0;
     char *line = NULL;
+    char stack[640 << 10];  // ought to be enough!
     size_t linecap = 0;
-    ssize_t linelen;
-    while ((linelen = getline(&line, &linecap, stdin)) > 0) {
-        int pos = 0;
-        while(pos < linelen) {
-            consume_chunk(line, &pos, linelen);
+    while (getline(&line, &linecap, stdin) > 0) {
+        int sp = 0;
+        for (char *c = line; *c != '\0' && *c != '\n'; c++) {
+            if (LPAREN(*c)) {
+                stack[sp++] = *c;
+            } else if (stack[sp - 1] == complement(*c)) {
+                sp--;
+            } else {
+                score += value(*c);
+                break;
+            }
         }
     }
 
-    printf("%d\n", total_points);
-
-    return 0;
+    printf("%d\n", score);
 }

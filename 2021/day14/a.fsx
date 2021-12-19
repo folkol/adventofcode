77b5eaa6fbd8f3331f82a09e0dc1b7b35ff7e280
Matte <mattias4@kth.se>
2021-12-19 22:50:47 +0100

    Minor rewrites

--- 2021/day14/a.fsx
+++ 2021/day14/a.fsx
@@ -3,20 +3,22 @@ let ruleParser (line:string) =
     | [| a; b |] -> a, b
     | _ -> failwith "Unexpected file format"
 
+let stitcher acc (a,b,c) =
+    match acc with
+    | "" -> a + b + c
+    | _ -> acc + b + c
+
 let lines = System.IO.File.ReadLines("input.dat")
 let mutable template = Seq.head <| lines
-let d = lines |> Seq.skip 2
+let rules = lines |> Seq.skip 2
               |> Seq.map ruleParser
               |>  dict
 
 for i in 1..10 do
     template <- template
         |> Seq.pairwise
-        |> Seq.map (fun (a,b) -> (string a), d.Item((string a) + (string b)), (string b))
-        |> Seq.fold (fun acc (a,b,c) ->
-            match acc with
-            | "" -> a + b + c
-            | _ -> acc + b + c) ""
+        |> Seq.map (fun (a,b) -> (string a), rules.Item((string a) + (string b)), (string b))
+        |> Seq.fold stitcher ""
 
 let counts = template |> Seq.countBy id |> Seq.map (fun (_, count) -> count)
 let min = counts |> Seq.min

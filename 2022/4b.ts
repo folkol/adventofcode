c6d03d26325bce92cfa72090497a1aa90f074822
Matte <mattias.johansson@funnel.io>
2022-12-04 14:28:43 +0100

    Don't need to trim here, the if will not break for the last, empty, line.

--- 2022/4b.ts
+++ 2022/4b.ts
@@ -3,7 +3,7 @@ import {strict as assert} from 'node:assert';
 
 let overlappingPairs = 0;
 let data = fs.readFileSync('4.dat', 'utf-8');
-data.trimEnd().split('\n').forEach(line => {
+data.split('\n').forEach(line => {
     let [aLo, aHi, bLo, bHi] = line.split(/[-,]/).map(Number)
     if (aHi >= bLo && aLo <= bHi) {
         overlappingPairs++;

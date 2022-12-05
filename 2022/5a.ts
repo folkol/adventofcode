8eb9abe50a9689a329744188a0bdcbc6aa318a43
Matte <mattias.johansson@funnel.io>
2022-12-05 08:47:03 +0100

    Replace {} with [] for stacks

--- 2022/5a.ts
+++ 2022/5a.ts
@@ -1,23 +1,20 @@
 import * as fs from "fs";
 import {strict as assert} from "node:assert"
 
-let stacks: Record<string, string[]> = {}
+let stacks: string[][] = [[], [], [], [], [], [], [], [], []]
 let lines = fs.readFileSync('5.dat', 'utf-8').trimEnd().split('\n');
 lines.forEach(line => {
     if (line.startsWith('move')) {
         let [n, from, to] = line.match(/\d+/g)?.map(Number) as number[];
-        stacks[to].push(...stacks[from].splice(-Number(n)).reverse());
+        stacks[to - 1].push(...stacks[from - 1].splice(-Number(n)).reverse());
     } else {
-        let crates = line.matchAll(/[A-Z]/g);
-        for (let crate of crates) {
-            let stack = 1 + (crate.index as number - 1) / 4;
-            stacks[stack] = stacks[stack] || []
-            stacks[stack].unshift(crate[0])
+        for (let i = 0; i * 4 < line.length; i++) {
+            if (line[i * 4] === '[') {
+                stacks[i].unshift(line[i * 4 + 1])
+            }
         }
     }
 })
-let ans = Object.entries(stacks).sort().map(entry => {
-    return entry[1].slice(-1)
-}).join('')
+let ans = stacks.map(stack => stack.slice(-1)).join('')
 console.log(ans)
 assert(ans === 'SVFDLGLWV')

09ad73c6e9b1411f187a9232da4d331f3f41c8f7
Matte <mattias.johansson@funnel.io>
2022-12-05 08:50:15 +0100

    Introduced redundant variable to avoid some confusing arithmetic

--- 2022/5a.ts
+++ 2022/5a.ts
@@ -8,9 +8,9 @@ lines.forEach(line => {
         let [n, from, to] = line.match(/\d+/g)?.map(Number) as number[];
         stacks[to - 1].push(...stacks[from - 1].splice(-Number(n)).reverse());
     } else {
-        for (let i = 0; i * 4 < line.length; i++) {
-            if (line[i * 4] === '[') {
-                stacks[i].unshift(line[i * 4 + 1])
+        for (let i = 0, stack = 0; i < line.length; stack += 1, i = stack * 4) {
+            if (line[i] === '[') {
+                stacks[stack].unshift(line[i + 1])
             }
         }
     }

905c71dffdbc6073698660aa65b549ac164546eb
Matte <mattias.johansson@funnel.io>
2023-12-03 22:19:20 +0100

    extracted some methods

--- 2023/day3/src/bin/a.rs
+++ 2023/day3/src/bin/a.rs
@@ -1,82 +1,103 @@
 use std::error::Error;
 use std::fs;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let data = fs::read_to_string("input.dat")?;
-    let lines: Vec<(usize, Vec<char>)> = data
-        .lines()
-        .map(|line| line.chars().collect())
-        .enumerate()
-        .collect();
-    let mut numbers: Vec<_> = Vec::new();
-    let mut part_numbers = Vec::new();
-    for (mut i, line) in &lines {
-        let mut begin = 0;
-        let mut end = 0;
-        let mut inside: bool = false;
-        for (j, c) in line.iter().enumerate() {
-            if c.is_ascii_digit() && !inside {
-                inside = true;
-                begin = j;
-                end = begin;
-            }
-            if c.is_ascii_digit() {
-                end = j;
-            }
-            if !c.is_ascii_digit() && inside {
-                numbers.push((i, begin, end, line.len()));
-                inside = false;
-            }
-        }
-        if inside {
-            numbers.push((i, begin, end, line.len()));
-        }
-        println!("{i:03}: {line:?}");
-        part_numbers.push(1)
-    }
+use regex;
+use regex::Regex;
 
-    let mut part_numbers: Vec<u32> = Vec::new();
-    'outer: for &(line, begin, end, llen) in &numbers {
-        let above = if line > 0 {
-            line - 1
-        } else {
-            line
-        };
-        let below = if line < lines.len() - 2 {
-            line + 1
-        } else {
-            line
-        };
-        let left = if begin > 0 {
-            begin - 1
-        } else {
-            begin
-        };
-        let right = if end < llen - 2 {
-            end + 1
-        } else {
-            end
-        };
-        for row in above..=below {
+struct Number {
+    value: u32,
+    lineno: usize,
+    begin: usize,
+    end: usize,
+}
+
+impl Number {
+    pub(crate) fn neighborhood(&self, width: usize, height: usize) -> Vec<(u32, u32)> {
+        let mut neighbours = Vec::new();
+        let top = self.lineno as i32 - 1;
+        let bottom = self.lineno as i32 + 1;
+        let left = self.begin as i32 - 1;
+        let right = self.end as i32 + 1;
+        let in_bounds = |i, j| i >= 0 && i < height as i32 && j >= 0 && j < width as i32;
+        for row in top..=bottom {
             for col in left..=right {
-                let c = lines[row].1[col];
-                if !c.is_ascii_digit() && c != '.' {
-                    let mut tmp = String::new();
-                    for n in begin..=end {
-                        tmp.push(lines[line].1[n]);
-                    }
-                    println!("Inspecting '{c}' @ ({row}, {col}), found symbol, adding {tmp}");
-                    part_numbers.push(tmp.parse()?);
-                    continue 'outer;
+                if in_bounds(row, col) {
+                    neighbours.push((row as u32, col as u32));
                 }
             }
         }
+        neighbours
     }
+}
 
-    let i1 = part_numbers.iter().sum::<u32>();
-    dbg!(i1);
+struct Schematic {
+    numbers: Vec<Number>,
+    width: usize,
+    height: usize,
+    data: String,
+}
 
-    assert_eq!(i1, 553825);
+impl Schematic {
+    pub(crate) fn get(&self, x: u32, y: u32) -> char {
+        let line = self.data.lines().nth(y as usize).unwrap();
+        line.chars().nth(x as usize).unwrap()
+    }
+}
 
-    Ok(())
+impl From<String> for Schematic {
+    fn from(data: String) -> Self {
+        let height = data.lines().count();
+        let width = data.lines().map(str::len).max().unwrap();
+        let numbers = Self::find_numbers(&data);
+        Schematic {
+            numbers,
+            height,
+            width,
+            data,
+        }
+    }
+}
+
+impl Schematic {
+    fn find_numbers(data: &str) -> Vec<Number> {
+        let pattern = Regex::new(r"\d+").unwrap();
+        let numbers_on_line = |(lineno, line)| {
+            pattern.find_iter(line).map(move |number| Number {
+                value: number.as_str().parse().unwrap(),
+                lineno,
+                begin: number.start(),
+                end: number.end() - 1,
+            })
+        };
+        data.lines()
+            .enumerate()
+            .flat_map(numbers_on_line)
+            .collect::<Vec<_>>()
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let schematic: Schematic = fs::read_to_string("input.dat")?.into();
+
+    let is_symbol = |(x, y)| {
+        let c: char = schematic.get(y, x);
+        !c.is_ascii_digit() && c != '.'
+    };
+
+    let is_part_number = |number: &&Number| {
+        number
+            .neighborhood(schematic.width, schematic.height)
+            .into_iter()
+            .any(is_symbol)
+    };
+
+    let part_numbers = schematic
+        .numbers
+        .iter()
+        .filter(is_part_number)
+        .map(|&Number { value, .. }| value);
+
+    let ans: u32 = part_numbers.sum();
+    assert_eq!(ans, 553825);
+    Ok(println!("{}", ans)) // 553825
 }
--- 2023/day3/src/bin/b.rs
+++ 2023/day3/src/bin/b.rs
@@ -1,115 +1,111 @@
 use std::error::Error;
 use std::fs;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let data = fs::read_to_string("input.dat")?;
-    let lines: Vec<(usize, Vec<char>)> = data
-        .lines()
-        .map(|line| line.chars().collect())
-        .enumerate()
-        .collect();
-    let mut numbers: Vec<_> = Vec::new();
-    let mut part_numbers = Vec::new();
-    for (mut i, line) in &lines {
-        let mut begin = 0;
-        let mut end = 0;
-        let mut inside: bool = false;
-        for (j, c) in line.iter().enumerate() {
-            if c.is_ascii_digit() && !inside {
-                inside = true;
-                begin = j;
-                end = begin;
-            }
-            if c.is_ascii_digit() {
-                end = j;
-            }
-            if !c.is_ascii_digit() && inside {
-                numbers.push((i, begin, end, line.len()));
-                inside = false;
-            }
-        }
-        if inside {
-            numbers.push((i, begin, end, line.len()));
-        }
-        println!("{i:03}: {line:?}");
-        part_numbers.push(1)
-    }
+use regex;
+use regex::Regex;
 
-    let mut part_numbers: Vec<u32> = Vec::new();
-    'outer: for &(line, begin, end, llen) in &numbers {
-        let above = if line > 0 {
-            line - 1
-        } else {
-            line
-        };
-        let below = if line < lines.len() - 2 {
-            line + 1
-        } else {
-            line
-        };
-        let left = if begin > 0 {
-            begin - 1
-        } else {
-            begin
-        };
-        let right = if end < llen - 2 {
-            end + 1
-        } else {
-            end
-        };
-        for row in above..=below {
+struct Number {
+    value: u32,
+    lineno: usize,
+    begin: usize,
+    end: usize,
+}
+
+impl Number {
+    fn neighborhood(&self, width: usize, height: usize) -> Vec<(u32, u32)> {
+        let mut neighbours = Vec::new();
+        let top = self.lineno as i32 - 1;
+        let bottom = self.lineno as i32 + 1;
+        let left = self.begin as i32 - 1;
+        let right = self.end as i32 + 1;
+        let in_bounds = |i, j| i >= 0 && i < height as i32 && j >= 0 && j < width as i32;
+        for row in top..=bottom {
             for col in left..=right {
-                let c = lines[row].1[col];
-                if !c.is_ascii_digit() && c != '.' {
-                    let mut tmp = String::new();
-                    for n in begin..=end {
-                        tmp.push(lines[line].1[n]);
-                    }
-                    println!("Inspecting '{c}' @ ({row}, {col}), found symbol, adding {tmp}");
-                    part_numbers.push(tmp.parse()?);
-
-                    continue 'outer;
+                if in_bounds(row, col) {
+                    neighbours.push((row as u32, col as u32));
                 }
             }
         }
+        neighbours
     }
+
+    fn adjacent(&self, square: (usize, usize), width: usize, height: usize) -> bool {
+        let row = square.0 as u32;
+        let col = square.1 as u32;
+        self.neighborhood(width, height).contains(&(row, col))
+    }
+}
+
+struct Schematic {
+    numbers: Vec<Number>,
+    width: usize,
+    height: usize,
+    data: String,
+}
+
+impl Schematic {
+    fn stars(&self) -> Vec<(usize, usize)> {
+        let mut stars = Vec::new();
+        for (row, line) in self.data.lines().enumerate() {
+            for (col, c) in line.chars().enumerate() {
+                if c == '*' {
+                    stars.push((row, col))
+                }
+            }
+        }
+        stars
+    }
+}
+
+impl From<String> for Schematic {
+    fn from(data: String) -> Self {
+        let height = data.lines().count();
+        let width = data.lines().map(str::len).max().unwrap();
+        let numbers = Self::find_numbers(&data);
+        Schematic {
+            numbers,
+            height,
+            width,
+            data,
+        }
+    }
+}
+
+impl Schematic {
+    fn find_numbers(data: &str) -> Vec<Number> {
+        let pattern = Regex::new(r"\d+").unwrap();
+        let numbers_on_line = |(lineno, line)| {
+            pattern.find_iter(line).map(move |number| Number {
+                value: number.as_str().parse().unwrap(),
+                lineno,
+                begin: number.start(),
+                end: number.end() - 1,
+            })
+        };
+        data.lines()
+            .enumerate()
+            .flat_map(numbers_on_line)
+            .collect::<Vec<_>>()
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let schematic: Schematic = fs::read_to_string("input.dat")?.into();
 
     let mut gear_ratios: Vec<u32> = Vec::new();
-    for (mut i, line) in &lines {
-        for (j, &c) in line.iter().enumerate() {
-            if c == '*' {
-                println!("found star at {i}, {j}");
-                let mut adjacent_numbers: Vec<u32> = Vec::new();
-                for &(line, begin, end, _) in &numbers {
-                    let line = line as i32;
-                    let i = i as i32;
-                    let j = j as i32;
-                    for n in begin..=end {
-                        let n = n as i32;
-                        let i2 = (line - i) * (line - i) + (n - j) * (n - j);
-                        let distance = (i2 as f64).sqrt();
-                        if distance < 1.44 {
-                            println!(" .. was adjacent to number at {line}, {begin}-{end} at a distance of {distance}");
-                            let mut tmp = String::new();
-                            for n in begin..=end {
-                                tmp.push(lines[line as usize].1[n as usize]);
-                            }
-                            adjacent_numbers.push(tmp.parse()?);
-                            break;
-                        }
-                    }
-                }
-                if adjacent_numbers.len() == 2 {
-                    gear_ratios.push(adjacent_numbers.iter().product());
-                }
-            }
+    for star in schematic.stars() {
+        let adjacent_part_numbers: Vec<_> = schematic
+            .numbers
+            .iter()
+            .filter(|number| number.adjacent(star, schematic.width, schematic.height))
+            .map(|Number { value, .. }| value)
+            .collect();
+        if adjacent_part_numbers.len() == 2 {
+            gear_ratios.push(adjacent_part_numbers.into_iter().product());
         }
     }
 
-    let i1 = gear_ratios.iter().sum::<u32>();
-    dbg!(i1);
-
-    assert_eq!(i1, 93994191);
-
-    Ok(())
+    let ans: u32 = gear_ratios.iter().sum();
+    assert_eq!(ans, 93994191);
+    Ok(println!("{}", ans)) // 553825
 }
